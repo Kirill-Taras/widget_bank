4cd36238914cab5d3f155cbba99d83cf914a5481
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-10-24 17:39:01 +0700

    fuction fix

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,13 @@
-def card_type_number(type_card: str, number_card: str) -> str:
-    return f"{type_card} {number_card[0:4]} {number_card[4:6]}** **** {number_card[-4:]}"
-
+def card_type_number(user_input: str) -> str:
+    split_str = user_input.split(" ")
+    if len(split_str[-1]) == 16:
+        return (
+               f"{' '.join(split_str[0:len(split_str) - 1])} "
+               f"{split_str[-1][0:4]} "
+               f"{split_str[-1][4:6]}** **** "
+               f"{split_str[-1][-4:]}"
+               )
+    elif len(split_str[-1]) == 20:
+        return f"{' '.join(split_str[0:len(split_str) - 1])} **{split_str[-1][-4:]}"
+    else:
+        return "Неверный номер счета или карты"
