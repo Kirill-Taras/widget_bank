bfa8df00ac9e32cd1f9909d398ef19596d84be28
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-10-24 18:02:26 +0700

    add fuction data_fix

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,13 @@
+from _datetime import datetime
+
+
 def card_type_number(user_input: str) -> str:
+    """
+    Функция принимает строку пользователя с данными
+    и приводит ее к стандартному зашифрованному виду.
+    :param user_input: строка с данными
+    # :return: Карта|Счет, номер карты|счета
+    """
     split_str = user_input.split(" ")
     if len(split_str[-1]) == 16:
         return (
@@ -11,3 +20,17 @@ def card_type_number(user_input: str) -> str:
         return f"{' '.join(split_str[0:len(split_str) - 1])} **{split_str[-1][-4:]}"
     else:
         return "Неверный номер счета или карты"
+
+
+def data_fix(user_input: str) -> str:
+    """
+    Функция получает от пользователя строку с информацией
+    даты и времени, и возвращает дату.
+    :param user_input: пользователь вводит дату и время
+    :return: день.месяц.год
+    """
+    the_data = datetime.fromisoformat(user_input)
+    return f"{the_data.day}.{the_data.month}.{the_data.year}"
+
+
+print(data_fix("2018-07-11T02:26:18.671407"))
