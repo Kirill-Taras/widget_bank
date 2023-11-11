f5666f494988475d1f67c393dfcb05858acea939
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-11-11 18:15:47 +0700

    homework_5 to correct

--- src/generators.py
+++ src/generators.py
@@ -10,9 +10,9 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Generator:
     :param currency: валюта.
     :yield: операция по заданной валюте.
     """
-    for i in transactions:
-        if i['operationAmount']['currency']['code'] == currency:
-            yield i
+    for operation in transactions:
+        if operation['operationAmount']['currency']['code'] == currency:
+            yield operation
 
 
 def transaction_descriptions(transactions: list[dict]) -> Generator:
@@ -22,19 +22,19 @@ def transaction_descriptions(transactions: list[dict]) -> Generator:
     :param transactions: список операций.
     :yield: описание операции.
     """
-    for i in transactions:
-        yield i["description"]
+    for operation in transactions:
+        yield operation["description"]
 
 
-def card_number_generator(first_i: int, last_i: int) -> Generator:
+def card_number_generator(first: int, last: int) -> Generator:
     """
     Функция, которая генерирует номера карт
     в формате "XXXX XXXX XXXX XXXX", где X — цифра.
-    :param first_i: От какой цифры начинается генерация.
-    :param last_i: До какой цифры идет генерация.
+    :param first: От какой цифры начинается генерация.
+    :param last: До какой цифры идет генерация.
     :yield: номер карты.
     """
-    for i in range(first_i, last_i + 1):
-        number: str = "0" * (16 - len(str(i))) + str(i)
+    for operation in range(first, last + 1):
+        number: str = "0" * (16 - len(str(operation))) + str(operation)
         number_card: str = " ".join([number[x: x + 4] for x in range(0, len(number), 4)])
         yield number_card
