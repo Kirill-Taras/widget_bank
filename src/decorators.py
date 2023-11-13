8613b9e6d24859ceff8eb5778f677d015ba9edf0
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-11-13 22:21:30 +0700

    homework_6 finish

--- src/decorators.py
+++ src/decorators.py
@@ -9,11 +9,11 @@ def log(filename: None | str = None) -> Callable:
         def inner(*args: Any, **kwargs: Any) -> Any:
             now: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             try:
-                result: Any = function(*args, **kwargs)
-                log_messenger: str = f"{now} {function.__name__} ok\n"
+                result = function(*args, **kwargs)
+                log_messenger= f"{now} {function.__name__} ok\n"
             except Exception as err:
-                result: None = None
-                log_messenger: str = (
+                result = None
+                log_messenger = (
                     f"{now} {function.__name__} " f"error: {type(err).__name__}. " f"Inputs: {args}, {kwargs}\n"
                 )
             if filename:
